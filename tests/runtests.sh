8e43e9ab991dc66f4607a085ffaf319d6181ae17
Zebulun Arendsee <arendsee@iastate.edu>
2017-01-16 13:30:59 -0600

    Cleaned up tests and added new options

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,21 +3,36 @@ set -u
 
 usage (){
 cat << EOF
-Test the final output of synder
-OPTIONAL ARGUMENTS
+Test suite for the LOC compiler
   -h  print this help message
   -q  quiet, print no output
+  -x  stop on first failure
+  -F  skip frontend tests
+  -B  skip backend tests
+  -K  skip tests of known problems
 EOF
     exit 0
 }
 
-quiet=0
-while getopts "hdqxvma:o:" opt; do
+loud=true
+instant_death=false
+test_frontend=true
+test_backend=true
+test_known_problems=true 
+while getopts "hqxFBK" opt; do
     case $opt in
         h)
             usage ;;
         q)
-            quiet=1 ;;
+            loud=false ;;
+        x)
+            instant_death=true ;;
+        F)
+            test_frontend=false ;;
+        B)
+            test_backend=false ;;
+        K)
+            test_known_problems=false ;;
         ?)
             warn "Illegal arguments"
             exit 1
@@ -28,8 +43,16 @@ n_fail=0
 n_pass=0
 loc=~/.loc/bin/loc
 
+say(){
+    $loud && echo "$@"
+}
+
+say_n(){
+    $loud && echo -n "$@"
+}
+
 announce(){
-    if [[ $quiet -eq 0 ]]
+    if $loud
     then
         [[ -t 1 ]] && o="\e[1;33m$1\e[0m" || o=$1
         echo -e $o
@@ -37,7 +60,7 @@ announce(){
 }
 
 warn(){
-    if [[ $quiet -eq 0 ]]
+    if $loud
     then
         [[ -t 1 ]] && o="\e[1;31m$1\e[0m" || o=$1
         echo -en $o
@@ -49,7 +72,7 @@ frontend_test(){
     msg=$2
     cd frontend-tests/$dir
 
-    echo -n "$msg"
+    say_n "$msg"
 
     $loc x.loc 2>&1 > /dev/null
     if [[ $? == 0 ]]
@@ -57,19 +80,20 @@ frontend_test(){
         diff <($loc x.loc) x.lil > /dev/null
         if [[ $? == 0 ]]
         then
-            echo OK
+            say OK
             n_pass=$(( n_pass + 1 ))
         else
             warn FAIL
-            echo " - bad LIL"
+            say " - bad LIL"
             n_fail=$(( n_fail + 1 ))
         fi
     else
         warn FAIL
-        echo " - non-zero exit status"
+        say " - non-zero exit status"
         n_fail=$(( n_fail + 1 ))
     fi
     cd - > /dev/null
+    $instant_death && exit 1
 }
 
 backend_test(){
@@ -79,7 +103,7 @@ backend_test(){
 
     cd backend-tests/$dir
     
-    echo -n "$msg"
+    say_n "$msg"
 
     loc -o tst x.loc 2>&1 > /dev/null
     if [[ $? == 0 ]]
@@ -92,30 +116,37 @@ backend_test(){
         diff $obs $exp > /dev/null
         if [[ $? == 0 ]]
         then
-            echo OK
+            say OK
             n_pass=$(( n_pass + 1 ))
         else
             warn FAIL
-            echo " - unexpected output"
+            say " - unexpected output"
             n_fail=$(( n_fail + 1 ))
         fi
         rm $obs $exp
     else
         warn FAIL
-        echo " - non-zero exit status"
+        say " - non-zero exit status"
         n_fail=$(( n_fail + 1 ))
     fi
     
     rm -rf tst
     cd - > /dev/null
+    $instant_death && exit 1
 }
 
+if $test_backend
+then
 announce "Backend tests"
 frontend_test elements      "elements/     -- basic manifold elements exist ... "
 frontend_test import        "import/       -- can import files ................ "
 frontend_test list-select   "list-select/  -- lists expand on couplet lhs ..... "
 frontend_test args          "args/         -- [+-]lhs, flags, lists ........... "
+fi
 
+
+if $test_frontend
+then
 announce "Frontend tests"
 backend_test sh-simple uniq 'sh-simple/    -- uniq . sort . grep . man ........ '
 backend_test sh-cached uniq 'sh-cached/    -- uniq . sort . grep . man ........ '
@@ -126,17 +157,23 @@ backend_test r-cached  sqrt 'r-cached/     -- sqrt . max . seq ................ 
 backend_test r-all     sqrt 'r-all/        -- sqrt . max . seq ................ '
 backend_test r-refer   max  'r-refer/      -- max . <runif> ................... '
 backend_test sh-and-r  grep 'sh-and-r/     -- grep . seq ...................... '
+fi
 
+if $test_known_problems
+then
 announce "Known problems"
 backend_test sh-race  cat   'sh-race/      -- cat . <random> <random> ......... '
+fi
 
 
 
-echo
+say
 if [[ $n_fail == 0 ]]
 then
-    echo All tests pass
+    say All tests pass
+    exit 0
 else
     warn "FAILURE"
-    echo " - $n_fail out of $(( n_fail + n_pass )) tests failed"
+    say " - $n_fail out of $(( n_fail + n_pass )) tests failed"
+    exit 1
 fi
