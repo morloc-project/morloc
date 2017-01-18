ee67aaf169238ed49edbc526614ce3c8899c07f3
Zebulun Arendsee <arendsee@iastate.edu>
2017-01-17 21:57:34 -0600

    Force evaluation in R null function
    The R null function is designed to be a terminal point for a pipeline
    that dumps inputs, so nothing is printed to STDOUT. However, my previous
    implementation
    
    ```
    null <- function(...) { NULL }
    ```
    
    had a serious problem. It did not force evaluation of inputs. In context
    a terminal null compiles to
    
    ```
    m0 <- function(){
        Mb <- null ( m1() )
        ...
    }
    ```
    
    If the pipeline is called by calling `null` (e.g. `manifold-nexus.sh
    null`), then null will be called and NULL returned, but since the
    arguments are not used, they will not be evaluated (since R is lazy).
    
    To fix this, I force evaluation:
    
    ```
    null <- function(...) { list(...); NULL }
    ```
    
    This fixes the 3rd known problem, `r-self-reference`.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -174,16 +174,17 @@ fi
 if $test_backend
 then
 announce "Backend tests"
-backend_test   sh-simple uniq 'sh-simple/         -- uniq . sort . grep . man ........ '
-backend_test   sh-cached uniq 'sh-cached/         -- uniq . sort . grep . man ........ '
-backend_test   sh-all    uniq 'sh-all/            -- uniq . sort . grep . man ........ '
-backend_test   sh-refer  head 'sh-refer/          -- head . <runif> .................. '
-backend_test   r-simple  sqrt 'r-simple/          -- sqrt . max . seq ................ '
-backend_test   r-cached  sqrt 'r-cached/          -- sqrt . max . seq ................ '
-backend_x_test r-all          'r-all/             -- sqrt . max . seq ................ '
-backend_test   r-refer   max  'r-refer/           -- max . <runif> ................... '
-backend_test   r-check   sqrt 'r-check/           -- sqrt . max . seq ................ '
-backend_test   sh-and-r  grep 'sh-and-r/          -- grep . seq ...................... '
+backend_test   sh-simple uniq    'sh-simple/         -- uniq . sort . grep . man ........ '
+backend_test   sh-cached uniq    'sh-cached/         -- uniq . sort . grep . man ........ '
+backend_test   sh-all    uniq    'sh-all/            -- uniq . sort . grep . man ........ '
+backend_test   sh-refer  head    'sh-refer/          -- head . <runif> .................. '
+backend_test   r-simple  sqrt    'r-simple/          -- sqrt . max . seq ................ '
+backend_test   r-cached  sqrt    'r-cached/          -- sqrt . max . seq ................ '
+backend_x_test r-all             'r-all/             -- sqrt . max . seq ................ '
+backend_test   r-refer   max     'r-refer/           -- max . <runif> ................... '
+backend_test   r-check   sqrt    'r-check/           -- sqrt . max . seq ................ '
+backend_test   sh-and-r  grep    'sh-and-r/          -- grep . seq ...................... '
+backend_x_test r-self-reference  'r-self-reference/  -- cat . <random> <random> ......... '
 fi
 
 if $test_known_problems
@@ -191,7 +192,6 @@ then
 announce "Known problems"
 backend_test   sh-race          cat 'sh-race/           -- cat . <random> <random> ......... '
 backend_test   r-single-quotes  say 'r-single-quotes/   -- cat . <random> <random> ......... '
-backend_x_test r-self-reference     'r-self-reference/  -- cat . <random> <random> ......... '
 backend_test   r-import         add 'r-import           -- fanciful import statement ....... '
 fi
 
