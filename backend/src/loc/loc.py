9c520410ede67b6d7ea04d06a7fd4f454028e94b
Zebulun Arendsee <arendsee@iastate.edu>
2017-02-01 14:22:10 -0600

    Clean interface and add typecheck call option

--- backend/src/loc/loc.py
+++ backend/src/loc/loc.py
@@ -13,9 +13,12 @@ __version__ = '0.0.0'
 __prog__ = 'loc'
 
 def parser(argv):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Compile a LOC program",
+        usage="loc [options] LOC_SCRIPT"
+    )
     parser.add_argument(
-        '--version',
+        '-v', '--version',
         help='Display version',
         action='version',
         version='%(prog)s {}'.format(__version__)
@@ -25,14 +28,20 @@ def parser(argv):
         help="LOC source file"
     )
     parser.add_argument(
+        '-t', '--typecheck',
+        help="Typecheck the LOC script (do not build)",
+        action='store_true',
+        default=False
+    )
+    parser.add_argument(
         '-m', '--print-manifolds',
-        help="help",
+        help="Print summaries of all manifolds",
         action='store_true',
         default=False
     )
     parser.add_argument(
         '-x', '--execution-path',
-        help="help"
+        help="Folder for caches and scripts"
     )
     parser.add_argument(
         '-k', '--clobber',
@@ -52,11 +61,24 @@ if __name__ == '__main__':
 
     args = parser(argv)
 
+    if args.typecheck:
+        type_errors = lil.typecheck(args.f)
+        if type_errors:
+            print(type_errors)
+            sys.exit(1)
+        else:
+            sys.exit(0)
+
     raw_lil = lil.compile_loc(args.f)
     exports = lil.get_exports(raw_lil)
     source = lil.get_src(raw_lil)
     manifolds = lil.get_manifolds(raw_lil)
     languages = set([l.lang for m,l in manifolds.items()])
+
+    if args.print_manifolds:
+        for k,m in manifolds.items():
+            m.print()
+        sys.exit(0)
 
     loc_home = os.path.expanduser("~/.loc")
     loc_tmp = "%s/tmp" % loc_home
@@ -87,10 +109,6 @@ if __name__ == '__main__':
                 pass
         else:
             err("Too many temporary directories")
-
-    if(args.print_manifolds):
-        for k,m in manifolds.items():
-            m.print()
 
     manifold_nexus = nexus.build_manifold_nexus(
         languages = languages,
