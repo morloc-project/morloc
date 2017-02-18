a99cb444472142fd4bd9e5dc4a421c91683d1d69
Zebulun Arendsee <arendsee@iastate.edu>
2017-02-18 00:41:52 -0600

    Added a cool test that broke everything ...

--- backend/src/loc/grammar/sh.py
+++ backend/src/loc/grammar/sh.py
@@ -175,7 +175,7 @@ fi
         )
 
     def make_input_manifold(self, m, pos, val, typ):
-        if(typ in ("Int", "String", "File", "Bool")):
+        if(typ in ("Int", "Num", "String", "File", "Bool")):
             op = '$'
         else:
             op = '<'
