3ea2b26b8bf868ccdf7a5547c730574d90cd34b4
Zebulun Arendsee <arendsee@iastate.edu>
2018-06-07 00:57:25 -0500

    Replace REPL with file parser
    Currently this file parser just reads one file, feeds it line by line
    into the Morloc interpreter, and shows the parse of each line.
    
    ```
    sum [1,2,3] ==> Right (Node (MFunc "sum") [Node (MInts [1,2,4,8]) []])
    1           ==> Right (Node (MInt 1) [])
    1 1         ==> Left "BadApplication: Primitives cannot take arguments\n"
    ```
    
    This is nice for debugging, but this isn't what we ultimately want.
    
    I need to settle on a syntax. It should be as minimal as it can be. None
    of the fanciness of the old C Morloc. Maybe just good, old fashioned,
    function application.

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
-import MorlocExecutable.Repl (repl)
+import System.Environment (getArgs)
+import Morloc (interpret)
 
-main :: IO ()
-main = repl
+scriptParser :: (String, String) -> String
+scriptParser (_, s) = (unlines . map show . map interpret) (lines s)
+
+interactWith function inputFiles = do
+  inputs <- mapM readFile inputFiles
+  putStrLn ((concat . map function) (zip inputFiles inputs))
+
+main = mainWith scriptParser
+  where mainWith f = do
+          args <- getArgs
+          case args of
+            [] -> putStrLn "You must provide at least one argument"
+            xs -> interactWith f xs
