11dd4123d5e0733015050fe8cef6eb7a8b6a3478
Zebulun Arendsee <arendsee@iastate.edu>
2018-07-26 17:00:34 -0500

    More schema
    I need to go full OWL. I want cardinality, among other things.
    Eventually, I want to do all my typechecking through declarative OWL
    statements. Then a program will be correct if it compiles into a
    consistent OWL system.
    
    I may need a bit of SPARQL to generate the triplets stating equivalence
    between things.
    
    I will also need a reasoning program. There is not currently a
    well-supported one in the Haskell ecosystem. I will probably need to
    make my own.

--- library/Morloc/RdfSchema.hs
+++ library/Morloc/RdfSchema.hs
@@ -12,12 +12,45 @@ sss :: DT.Text -> DT.Text -> DT.Text -> DR.Triple
 sss s p o = DR.triple (DR.UNode s) (DR.UNode p) (DR.UNode o)
 
 rdfSchema =
-  [ sss "morloc:list"                 "rdfs:subClassOf" "rdfs:Seq"
-  , sss "morloc:tuple"                "rdfs:subClassOf" "rdfs:Seq"
-  , sss "morloc:record"               "rdfs:subClassOf" "rdfs:Bag"
-  , sss "morloc:script"               "rdfs:subClassOf" "rdfs:Bag"
-  , sss "morloc:call"                 "rdfs:subClassOf" "rdfs:Seq"
-  , sss "morloc:input"                "rdfs:subClassOf" "rdfs:Seq"
-  , sss "morloc:parameterizedType"    "rdfs:subClassOf" "rdfs:Seq"
-  , sss "morloc:parameterizedGeneric" "rdfs:subClassOf" "rdfs:Seq"
+  [
+  -- collections
+    sss "morloc:list"                 "rdfs:subClassOf" "rdfs:Seq" -- elements
+  , sss "morloc:tuple"                "rdfs:subClassOf" "rdfs:Seq" -- elements
+  , sss "morloc:record"               "rdfs:subClassOf" "rdfs:Bag" -- tag/value pairs
+  , sss "morloc:script"               "rdfs:subClassOf" "rdfs:Bag" -- statements
+  , sss "morloc:call"                 "rdfs:subClassOf" "rdfs:Seq" -- arguments
+  , sss "morloc:parameterizedType"    "rdfs:subClassOf" "rdfs:Seq" -- type parameters
+  , sss "morloc:parameterizedGeneric" "rdfs:subClassOf" "rdfs:Seq" -- type parameters
+  , sss "morloc:restricted_import"    "rdfs:subClassOf" "rdfs:Bag" -- imports
+  , sss "morloc:import"               "rdfs:subClassOf" "rdfs:Bag" -- imports
+  , sss "morloc:source"               "rdfs:subClassOf" "rdfs:Bag" -- imports
+  , sss "morloc:functionType"         "rdfs:subClassOf" "rdfs:Seq" -- inputs
+  -- primitives
+  , sss "morloc:alias"         "rdf:type" "xsd:string"
+  , sss "morloc:atomicGeneric" "rdf:type" "xsd:string"
+  , sss "morloc:atomicType"    "rdf:type" "xsd:string"
+  , sss "morloc:integer"       "rdf:type" "xsd:integer"
+  , sss "morloc:boolean"       "rdf:type" "xsd:boolean"
+  , sss "morloc:number"        "rdf:type" "xsd:float"
+  , sss "morloc:key"           "rdf:type" "xsd:string"
+  , sss "morloc:label"         "rdf:type" "xsd:string"
+  , sss "morloc:lang"          "rdf:type" "xsd:string"
+  , sss "morloc:name"          "rdf:type" "xsd:string"
+  , sss "morloc:string"        "rdf:type" "xsd:string"
+  , sss "morloc:namespace"     "rdf:type" "xsd:string"
+  -- generic value tags
+  , sss "morloc:value"  "rdf:type" "rdf:value"
+  , sss "morloc:output" "rdf:type" "rdf:value"
+  , sss "morloc:lhs"    "rdf:type" "rdf:value"
+  , sss "morloc:rhs"    "rdf:type" "rdf:value"
+  -- -- things with a left and right hand side (binary operators)
+  -- "morloc:binop"
+  -- "morloc:composition"
+  -- "morloc:dataDeclaration"
+  -- "morloc:namedType"
+  -- "morloc:recordEntry"
+  -- "morloc:typeDeclaration"
+  --
+  -- -- unary operators
+  -- "morloc:unaryOp"
   ]
