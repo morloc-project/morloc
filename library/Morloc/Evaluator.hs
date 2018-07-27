9b874e68555adcc756d0be9c99e4c962110b1835
Zebulun Arendsee <arendsee@iastate.edu>
2018-07-27 00:33:05 -0500

    Add some stubby material to the evaluator
    I'm still undecided on whether to bring back the data structures. I'll
    tinker with the RDF querying for a few more days and see how it goes.

--- library/Morloc/Evaluator.hs
+++ library/Morloc/Evaluator.hs
@@ -1,10 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Morloc.Evaluator
 (
-  rdf2manifolds
+    rdf2manifolds
+  , isElement
+  , position
+  , qSeq
 ) where
 
 import qualified Data.RDF as DR
+import qualified Data.Text as DT
+import qualified Data.Char as DC
 import qualified Morloc.Data as MD
 
 rdf2manifolds :: DR.RDF a -> [MD.Manifold]
 rdf2manifolds = undefined
+
+isElement :: DR.Node -> Bool
+isElement (DR.UNode s)
+  = maybe False id             -- Bool
+  . fmap (DT.all DC.isNumber)  -- Maybe Bool
+  . DT.stripPrefix "rdf:_"     -- Maybe Text
+  $ s
+isElement _ = False
+
+position :: DR.Node -> Maybe Int
+position (DR.UNode s) = case DT.stripPrefix "rdf:_" s of
+  Just str_int -> case (reads (show str_int) :: [(Int, String)]) of
+    [(i, "")] -> Just i
+    _ -> Nothing
+  _ -> Nothing
+position _ = Nothing
+
+qSeq :: DR.Rdf a => DR.Object -> DR.RDF a -> [DR.Triple]
+qSeq obj rel = DR.select
+  rel
+  Nothing
+  (Just isElement)
+  (Just ((==) obj))
