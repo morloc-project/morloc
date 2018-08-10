7cb7047025073c00903da30f3677d06710e29661
Zebulun Arendsee <arendsee@iastate.edu>
2018-08-10 05:52:04 -0500

    Simplify packers
    The previous commit overcomplicated the problem. The serialization
    functions can have the forms:
    
    ```
    <name> :: packs => <lang>/<type> -> Common
    <name> :: unpacks => Common -> <lang>/<type>
    ```
    
    In order to find the appropriate function, the compiler needs to have a
    mapping from the language-specific type, `l a`, to the general type,
    `b`.
    
    I've also moved to the syntax:
    
    ```
    {:
        <subject> <predicate> <object> .
        <subject> <predicate> <object> .
    :}
    ```
    
    For representing arbitrary relations. This is could benefit from some
    Turtle-style sugar, but is fine for now.

--- ontology/R-types.R
+++ ontology/R-types.R
@@ -4,13 +4,15 @@ library(data.table)
 # Extract R objects from JSON text
 unpackDataFrame <- function(json){ as.data.frame(fromJSON(json)) }
 unpackDataTable <- function(json){ as.data.table(fromJSON(json)) }
-unpackVector    <- fromJSON
-unpackList      <- function(json) {fromJSON(json, simplifyVector = FALSE)}
-unpackMatrix    <- function(json){ fromJSON(json, simplifyVector = FALSE, simplifyMatrix = TRUE) }
+unpackVector <- fromJSON
+unpackList <- function(json) {fromJSON(json, simplifyVector = FALSE)}
+unpackMatrix <- function(json){
+  fromJSON(json, simplifyVector = FALSE, simplifyMatrix = TRUE)
+}
 
 # Serialize R objects into JSON text
 packDataFrame <- function(json){ toJSON(json, dataframe="columns") }
-packDataTable <- packData.Frame
+packDataTable <- packDataFrame
 packVector    <- toJSON
 packList      <- toJSON
 packMatrix    <- toJSON
