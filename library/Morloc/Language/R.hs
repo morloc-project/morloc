4b080db3da88cd71ee982ee2dbc7c291cc3e383f
Zebulun Arendsee <arendsee@iastate.edu>
2018-08-26 17:16:34 -0500

    Create stubs exported manifolds

--- library/Morloc/Language/R.hs
+++ library/Morloc/Language/R.hs
@@ -13,6 +13,7 @@ module Morloc.Language.R (generate) where
 
 import Morloc.Quasi
 import Morloc.Types
+import qualified Morloc.System as MS
 import qualified Morloc.Util as MU
 import qualified Morloc.Query as Q
 
@@ -26,8 +27,37 @@ generate e
   <*> pure "R"
   <*> generateCode e
 
+expsQ :: SparqlEndPoint -> IO [[Maybe DT.Text]]
+expsQ = [sparql|
+  PREFIX mlc: <http://www.morloc.io/ontology/000/>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+  SELECT ?fname ?typedec (count (distinct ?element) as ?nargs)
+  WHERE {
+    ?typedec rdf:type mlc:typeDeclaration ;
+       mlc:lang "Morloc" ;
+       mlc:lhs ?fname ;
+       mlc:rhs ?type .
+
+    ?type rdf:type mlc:functionType .
+
+    ?arg ?element ?type .
+    FILTER(regex(str(?element), "_[0-9]+$", "i"))
+
+    ?src rdf:type mlc:source ;
+         mlc:lang "R" ;
+         mlc:import ?i .
+
+    ?i mlc:alias ?fname .
+
+    ?e rdf:type mlc:export ;
+       rdf:value ?fname .
+  }
+  GROUP BY ?typedec ?fname
+|]
+
 generateCode :: SparqlEndPoint -> IO DT.Text
-generateCode e = fmap render (main <$> srcs' <*> names')
+generateCode e = fmap render (main <$> srcs' <*> exps')
   where
     srcs' :: IO [Doc]
     srcs' = fmap (map toOne) (Q.sourcesByLangQ (dquotes "R") e)
@@ -35,19 +65,24 @@ generateCode e = fmap render (main <$> srcs' <*> names')
     toOne :: [Maybe DT.Text] -> Doc
     toOne [Just x] = text' x
     toOne _ = error "Bad SPARQL return"
-    
-    toRSources xs = MU.unique [p | (x,_,_,p) <- xs , x == "R"] 
 
-    names' :: IO [Doc]
-    names' = return ["foo"]
+    exps' :: IO [(Doc, Doc, [Doc])]
+    exps' = fmap (map toExps) (expsQ e)
+
+    toExps :: [Maybe DT.Text] -> (Doc, Doc, [Doc])
+    toExps [Just fname, Just uid, Just nargs]
+      = ( text' fname
+        , text' (MS.makeManifoldName uid)
+        , map (<> "x") (map int [1 .. (read (DT.unpack nargs) :: Int)])
+        )
 
 
-main :: [Doc] -> [Doc] -> Doc
-main srcs names = [idoc|#!/usr/bin/env Rscript
+main :: [Doc] -> [(Doc, Doc, [Doc])] -> Doc
+main srcs exps = [idoc|#!/usr/bin/env Rscript
 
 ${vsep (map sourceT srcs)}
 
-${vsep (map functionT names)}
+${vsep (map exportedT exps)}
 
 args <- commandArgs(trailingOnly=TRUE)
 if(length(args) == 0){
@@ -67,9 +102,10 @@ if(length(args) == 0){
 
 sourceT s = [idoc|source("${s}")|]
 
-functionT s = [idoc|
-# comment
-mXX <- function(...){
-  ${s}(...)
+
+exportedT (fname, uid, args) = [idoc|
+# ${fname}
+m${uid} <- function(${hcat (punctuate ", " args)}){
+  mXX(${hcat (punctuate ", " args)})
 }
 |]
