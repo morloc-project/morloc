b0e036874df0a08a3c3a055c996e9c9b9cc83471
Zebulun Arendsee <arendsee@iastate.edu>
2018-09-03 00:34:56 -0500

    Replace clunky option handling with docopt magic
    `docopt` is THE RIGHT WAY to do command line argument handling. The
    general idea of `docopt` is that the usage statements conventional in
    the UNIX world are formalized enough to be parsed directly into working
    argument handling code. There are implementations of `docopt` in most
    major languages.

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,38 +1,48 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
-module Main (main) where
+module Main where
 
 import qualified Morloc as M
-import qualified System.Environment as SE
+import System.Console.Docopt
+import Control.Monad (when)
 import qualified Data.Text as DT
 import qualified Data.Text.IO as DTI
+import qualified System.Environment as SE
 
-usage :: IO ()
-usage = putStr $ unlines [
-      "The morloc CLI is under development, try one of these patterns:"
-    , "  morloc --rdf -e <code>"
-    , "  morloc --rdf --triple -e <code>"
-    , "  morloc --rdf <filename>"
-    , "  morloc --rdf --triple <filename>"
-    , "  morloc <filename> <sparql-endpoint>"
-  ]
+patterns :: Docopt
+patterns = [docopt|
+morloc version 0.14.0
+
+Usage:
+  morloc make [--expression] <script> <sparql-endpoint>
+  morloc rdf [--triple] [--expression] <script>
+
+Options:
+  -t, --triple       print RDF graph in triple format, rather than the turtle
+  -e, --expression   read script as string rather than file
+|]
+
+getArgOrExit = getArgOrExitWith patterns
 
 main :: IO ()
 main = do
-  args <- SE.getArgs
-  case args of
-    -- no input
-    []  -> usage
+  args <- parseArgsOrExit patterns =<< SE.getArgs
 
-    ["--rdf", "-e", text] -> M.writeTurtle (DT.pack text)
+  -- do the following if we are processing Morloc code
+  when (isPresent args (argument "script")) $ do
+    -- handle the code, either from a file or a raw string
+    script <- if isPresent args (longOption "expression")
+              then (fmap DT.pack $ getArgOrExit args (argument "script"))
+              else getArgOrExit args (argument "script") >>= DTI.readFile
 
-    ["--rdf", "--triple", "-e", text] -> M.writeTriple (DT.pack text)
+    -- build a Morloc program, generating the nexus and pool files
+    when (isPresent args (command "make")) $ do
+      ep <- getArgOrExit args (argument "sparql-endpoint")
+      M.writeProgram ep script
 
-    ["--rdf", x] -> DTI.readFile x >>= M.writeTurtle
-
-    ["--rdf", "--triple", x] -> DTI.readFile x >>= M.writeTriple
-
-    [x, endpoint] -> DTI.readFile x >>= M.writeProgram endpoint
-
-    -- wrong input
-    _   -> usage
+    -- compile a Morloc script to RDF (turtle format by default)
+    when (isPresent args (command "rdf")) $ do
+      let writer = if isPresent args (longOption "triple") 
+                   then M.writeTriple
+                   else M.writeTurtle
+      writer script
