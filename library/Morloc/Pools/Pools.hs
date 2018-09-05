6387b9e3599c3b1f1db7d2c4f574622f715f1359
Zebulun Arendsee <arendsee@iastate.edu>
2018-09-05 15:19:06 -0500

    Remove unused Query.hs module and other

--- library/Morloc/Pools/Pools.hs
+++ library/Morloc/Pools/Pools.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes #-}
 
 {-|
 Module      : Morloc.Pools.Pools
@@ -13,14 +13,14 @@ module Morloc.Pools.Pools (generate) where
 
 import Morloc.Types
 import Morloc.Operators
-import Morloc.Query as Q
+import Morloc.Quasi
 import qualified Data.Text as DT
 
 import qualified Morloc.Pools.Template.R as RLang
 import qualified Morloc.Pools.Template.Python3 as Py3
 
 generate :: SparqlEndPoint -> IO [Script]
-generate ep = Q.languagesQ ep >>= foo' where 
+generate ep = languagesQ ep >>= foo' where 
   foo' :: [[Maybe DT.Text]] -> IO [Script]
   foo' xss = sequence (map (generateLang ep) xss)
 
@@ -30,3 +30,15 @@ generateLang e lang = case lang of
   [Just "py"] -> Py3.generate e
   [Just x] -> error ("The language " ++ show x ++ " is not supported")
   x -> error ("Bad SPARQL query:" ++ show x)
+
+languagesQ :: SparqlEndPoint -> IO [[Maybe DT.Text]]
+languagesQ = [sparql|
+  PREFIX mlc: <http://www.morloc.io/ontology/000/>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+  SELECT DISTINCT ?lang
+  WHERE {
+    ?i rdf:type mlc:source ;
+       mlc:lang ?lang .
+  }
+|]
