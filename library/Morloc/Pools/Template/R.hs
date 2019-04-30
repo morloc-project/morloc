836c18da92dbdc9842e0fda35070d5e86dee364b
Zebulun Arendsee <arendsee@iastate.edu>
2019-04-30 17:23:19 -0500

    Hack a solution to the py import from R
    See note in R template ...

--- library/Morloc/Pools/Template/R.hs
+++ library/Morloc/Pools/Template/R.hs
@@ -107,7 +107,8 @@ main
   :: [Doc] -> [Manifold] -> SerialMap -> MorlocMonad Doc
 main srcs manifolds hash = do
   lib <- fmap text' $ MM.asks MC.configLibrary
-  let sources = line <> vsep (map ((gImport g) lib) srcs)
+  -- TODO: fix this hack - the sources should be filtered by language before being passed to main
+  let sources = line <> vsep (map ((gImport g) lib) (filter (\s -> MT.isSuffixOf ".R" (render s)) srcs))
   sourceManifolds <- makeSourceManifolds g hash manifolds
   cisManifolds <- makeCisManifolds g hash manifolds
   mids <- MM.mapM callIdToName manifolds
