44a01f72ed6498e5e00cecce592a4e952e3accf6
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-05 05:51:34 -0500

    Filled out C grammar (with FIXMES)
    Here are the main challenges to generating C code:
     1. types in assignments and prototypes
     2. memory management - allocation (which is type dependent) and memory freeing
     3. compilation - which may require custom compiler flags
     4. pointers and casting
    
    A function declaration now requires the return type, the type of each
    argument and the creation of a prototype elsewhere in the code.
    
    I think I will for now abandon my attempt to use the same Grammar record
    for R/Python and C. Instead I will work on an independent C generator
    and then abstract it to work on other typed, non-garbage collecting,
    languages (e.g., fortran).

--- library/Morloc/Pools/Template/C.hs
+++ library/Morloc/Pools/Template/C.hs
@@ -21,12 +21,98 @@ import Morloc.Global
 import qualified Morloc.Data.Text as MT
 import Morloc.Data.Doc hiding ((<$>))
 import Morloc.Quasi
+import Morloc.Pools.Common
 
 generate :: SparqlDatabaseLike db => db -> MorlocMonad Script
-generate _ = Script <$> pure "pool" <*> pure CLang <*> pure stubCode
+generate db = makeGenerator g (defaultCodeGenerator g asImport) db
 
-stubCode :: MT.Text
-stubCode = render [idoc|
+asImport :: MT.Text -> MorlocMonad Doc
+asImport s = return . text' $ s
+
+-- generate :: SparqlDatabaseLike db => db -> MorlocMonad Script
+-- generate _ = Script <$> pure "pool" <*> pure CLang <*> pure stubCode
+
+g = Grammar {
+      gLang        = CLang
+    , gAssign      = assign' 
+    , gCall        = call'
+    , gFunction    = function'
+    , gId2Function = id2function'
+    , gComment     = comment'
+    , gReturn      = return'
+    , gQuote       = dquotes
+    , gImport      = include'
+    , gTrue        = "1" 
+    , gFalse       = "0"
+    , gList        = array'
+    , gTuple       = array'
+    , gRecord      = struct'
+    , gIndent      = indent'
+    , gTry         = tryCmd -- do or do not, there is no try
+    , gUnpacker    = unpacker'
+    , gForeignCall = foreignCall'
+    , gHash        = hash'
+    , gMain        = main'
+  } where
+
+    indent' = indent 4
+
+    -- FIXME: need to specify type
+    -- FIXME: l is dependent on r, e.g.:
+    --   int xs[5] = {1,2,3,4,5};
+    -- FIXME: memory management
+    assign' :: Doc -> Doc -> Doc
+    assign' l r = l <> " = " <> r
+
+    call' :: Doc -> [Doc] -> Doc
+    call' f args = f <> tupled args
+
+    -- FIXME: need to specity return type
+    -- FIXME: need to specify each argument type
+    function' :: Doc -> [Doc] -> Doc -> Doc
+    function' name args body
+      = "<type> " <> name <> tupled args <> braces (line <> indent' body <> line)
+
+    id2function' :: Integer -> Doc
+    id2function' i = "m" <> (text' (MT.show' i))
+
+    comment' :: Doc -> Doc
+    comment' d = "// " <> d
+
+    return' :: Doc -> Doc
+    return' x = call' "return" [x]
+
+    -- FIXME: needs to allocate memory
+    -- FIXME: needs type
+    array' :: [Doc] -> Doc
+    array' xs = encloseSep "{" "}" ", " xs 
+
+    -- FIXME: needs declaration
+    -- FIXME: needs memory management
+    -- FIXME: needs type for assignment
+    struct' :: [(Doc,Doc)] -> Doc
+    struct' xs = encloseSep "{" "}" ", " (map (\(k,v) -> "." <> k <> "=" <> v) xs)
+
+    -- FIXME: distinguish between `<lib.h>` and `"lib.h"` imports
+    include' :: Doc -> Doc -> Doc
+    include' _ s = "#include " <> dquotes s
+
+    -- FIXME: add error handling
+    unpacker' :: UnpackerDoc -> Doc
+    unpacker' u = call' (udUnpacker u) [udValue u]
+
+    -- FIXME: implement `_morloc_foreign_call`
+    foreignCall' :: ForeignCallDoc -> Doc
+    foreignCall' f = call' "_morloc_foreign_call" (map dquotes (fcdCliArgs f))
+
+    -- FIXME: I am not sure we even need this in C, the purpose for which it is
+    -- used in R/Python is served in C with a switch statement.
+    hash' :: (Manifold -> Doc) -> (Manifold -> Doc) -> [Manifold] -> Doc
+    hash' _ _ _ = ""
+
+    main' :: [Doc] -> Doc -> Doc -> Doc -> Doc -> MorlocMonad Doc
+    main' _ _ _ _ _ = return $ [idoc|
+
 #include <string.h>
 
 #include <stdio.h>
