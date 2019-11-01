7b78aade8493fbd70076b33167e0a5e60aa044d4
Zebulun Arendsee <arendsee@iastate.edu>
2019-11-01 00:42:08 -0500

    Add R golden test
    This test runs fine locally, but to make it work on travis I will need
    to get R installed.

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,14 +1,18 @@
 import Test.Tasty
+import qualified System.Directory as SD
 
 import PropertyTypeTests (propertyTypeTests)
 import UnitTypeTests (unitTypeTests)
 import GoldenMakefileTests (goldenMakefileTest)
 
 main = do
+  wd <- SD.getCurrentDirectory >>= SD.makeAbsolute
+  let golden = \msg f -> goldenMakefileTest msg (wd ++ "/test-suite/golden-tests/" ++ f)
   defaultMain $
     testGroup
       "Morloc tests"
-      [ goldenMakefileTest "Golden test #1" "test-suite/tests-data"
+      [ golden "Golden test for R" "2_r"
+      , golden "Golden test for C++" "1_cpp"
       , unitTypeTests
       , propertyTypeTests
       ]
