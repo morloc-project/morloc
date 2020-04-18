e168c1bd98f3ae52b1394df8b07f8763a22bf9ff
Zebulun Arendsee <arendsee@iastate.edu>
2020-04-17 22:45:13 -0500

    (22/207 fail) add R and C++ argument tests

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -14,16 +14,42 @@ main = do
       [ unitTypeTests
       , typeOrderTests
       , propertyTypeTests
+
+      , golden "argument-form-1-c" "argument-form-1-c"
       , golden "argument-form-1-py" "argument-form-1-py"
+      , golden "argument-form-1-r" "argument-form-1-r"
+
+      , golden "argument-form-2-c" "argument-form-2-c"
       , golden "argument-form-2-py" "argument-form-2-py"
+      , golden "argument-form-2-r" "argument-form-2-r"
+
+      , golden "argument-form-3-c" "argument-form-3-c"
       , golden "argument-form-3-py" "argument-form-3-py"
+      , golden "argument-form-3-r" "argument-form-3-r"
+
+      , golden "argument-form-4-c" "argument-form-4-c"
       , golden "argument-form-4-py" "argument-form-4-py"
+      , golden "argument-form-4-r" "argument-form-4-r"
+
+      , golden "argument-form-5-c" "argument-form-5-c"
       , golden "argument-form-5-py" "argument-form-5-py"
+      , golden "argument-form-5-r" "argument-form-5-r"
+
+      , golden "argument-form-6-c" "argument-form-6-c"
       , golden "argument-form-6-py" "argument-form-6-py"
+      , golden "argument-form-6-r" "argument-form-6-r"
+
+      , golden "argument-form-7-c" "argument-form-7-c"
       , golden "argument-form-7-py" "argument-form-7-py"
+      , golden "argument-form-7-r" "argument-form-7-r"
+
+      , golden "argument-form-8-c" "argument-form-8-c"
       , golden "argument-form-8-py" "argument-form-8-py"
+      , golden "argument-form-8-r" "argument-form-8-r"
+
       , golden "interop-1-py" "interop-1-py"
       , golden "interop-1-r" "interop-1-r"
+
       , golden "manifold-form-0" "manifold-form-0"
       , golden "manifold-form-0x" "manifold-form-0x"
       , golden "manifold-form-1" "manifold-form-1"
@@ -43,6 +69,7 @@ main = do
       , golden "manifold-form-7_c" "manifold-form-7_c"
       , golden "manifold-form-7_py" "manifold-form-7_py"
       , golden "manifold-form-7_r" "manifold-form-7_r"
+
       , golden "records-1-c" "records-1-c"
       , golden "records-1-py" "records-1-py"
       , golden "records-1-r" "records-1-r"
