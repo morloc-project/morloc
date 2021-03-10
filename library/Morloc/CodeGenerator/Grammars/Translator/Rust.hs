5a0544960d37a65e54110c8712e04dbcffaf1dd3
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-10 14:14:54 -0500

    Added Rust example

--- library/Morloc/CodeGenerator/Grammars/Translator/Rust.hs
+++ library/Morloc/CodeGenerator/Grammars/Translator/Rust.hs
@@ -9,6 +9,30 @@ Maintainer  : zbwrnz@gmail.com
 Stability   : experimental
 -}
 
+{-
+use super::serial;
+use std::env;
+
+pub fn m0(x0: &str) -> String {
+    let a0: String = serial::deserialize(x0).into();
+    let a1 = morloc_id(&a0);
+    let a2 = serial::serialize(&a1);
+    a2
+}
+
+fn main() {
+    let args = env::args();
+    args.next();
+    let cmd_id: u64 = args.next().unwrap().parse().unwrap();
+    let result;
+    match cmd_id {
+        0 => result = m0(&args.next().unwrap()),
+        _ => panic!("invalid function!")
+    }
+    println!("{}", result);
+}
+ - -}
+
 module Morloc.CodeGenerator.Grammars.Translator.Rust
   ( 
     translate
