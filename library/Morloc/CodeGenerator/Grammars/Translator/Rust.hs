ee592c4df8260810a09461a4cbc83f7a9da20980
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-10 15:10:23 -0500

    Added another Rust example

--- library/Morloc/CodeGenerator/Grammars/Translator/Rust.hs
+++ library/Morloc/CodeGenerator/Grammars/Translator/Rust.hs
@@ -9,7 +9,7 @@ Maintainer  : zbwrnz@gmail.com
 Stability   : experimental
 -}
 
-{-
+{- Example 1
 use super::serial;
 use std::env;
 
@@ -27,6 +27,56 @@ fn main() {
     let result;
     match cmd_id {
         0 => result = m0(&args.next().unwrap()),
+        _ => panic!("invalid function!")
+    }
+    println!("{}", result);
+}
+ - -}
+
+{- Example 2
+use std::env;
+use std::process::{Command, Stdio};
+
+use super::serial;
+
+fn foreign_call(cmd: &str, args: &[&str]) -> String {
+    let mut cmd = Command::new(cmd);
+    for arg in args {
+        cmd.arg(*arg);
+    }
+    cmd.stdout(Stdio::piped());
+    cmd.spawn().unwrap().wait().unwrap();
+    String::from_utf8(cmd.output().unwrap().stdout).unwrap()
+}
+
+// Translated from the python file
+
+// py2c x y = add (mul x y) 100
+fn m0(x0: &str, x1: &str) -> String {
+    let a0: f64 = serial::deserialize(x0).into();
+    let a1: f64 = serial::deserialize(x1).into();
+    let a2 = morloc_add(a0, a1);
+    let a3 = serial::serialize(a2);
+    a3
+}
+
+// c2py x y = mul (add x y) 100
+fn m1(x0: &str, x1: &str) -> String {
+    let a0 = foreign_call("./pool-cpp.out", &["13", x0, x1]);
+    let a1: f64 = serial::deserialize(x0).into();
+    let a2 = morloc_add(a1, 100.0);
+    let a3 = serial::serialize(a2);
+    a3
+}
+
+fn main() {
+    let args = env::args();
+    args.next();
+    let cmd_id: u64 = args.next().unwrap().parse().unwrap();
+    let result;
+    match cmd_id {
+        0 => result = m0(&args.next().unwrap(), &args.next().unwrap()),
+        1 => result = m1(&args.next().unwrap(), &args.next().unwrap()),
         _ => panic!("invalid function!")
     }
     println!("{}", result);
