1df513bac7b86cf919cbb82ea5d69d7328a4e020
Zebulun Arendsee <zbwrnz@gmail.com>
2021-07-25 15:48:40 -0500

    Add a non-injective, non-surjective, single value copy map (GMap)
    `GMap` has the type `GMap (Map a b) (Map b c)`.
    
    The first map maps to keys of the second map. The initial use case is to
    store links to a type annotations shared by many terms. This reduces
    duplication in memory and allows easy unification of terms. For example,
    
    ```
    x :: A
    
    foo y = baz (bar x) x
    
    bar z = z + 1
    ```
    
    Here both `x` terms in the definition of `foo` and the `z` in `bar` are
    all linked to the type `A`. When we see that `x` is passed to `bar`, we
    can set the type annotation of `z` to the same as `x`.
    
    Or we can take a SAnno object and reindex it by creating new indexes and
    linking them to whatever the old indices pointed to.

---
+++ library/Morloc/Data/GMap.hs
@@ -0,0 +1,73 @@
+{-|
+Module      : Morloc.Data.GMap
+Description : A general map datatype (non-injective and non-surjective)
+Copyright   : (c) Zebulun Arendsee, 2021
+License     : GPL-3
+Maintainer  : zbwrnz@gmail.com
+Stability   : experimental
+-}
+
+module Morloc.Data.GMap
+  ( GMap(..)
+  , elems
+  , empty
+  , innerKeys
+  , insert
+  , keys
+  , lookup
+  , mapInnerKeys
+  , mapKeys
+  , mapVals
+  , yIsX
+  ) where
+
+import Prelude hiding (lookup)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+data GMap a b c = GMap (Map.Map a b) (Map.Map b c)
+
+data GMapRet c
+  = GMapNoFst -- ^ Failure on the first key
+  | GMapNoSnd -- ^ Failure on the internal key (possible bug)
+  | GMapJust c
+
+mapVals :: (c -> c') -> GMap a b c -> GMap a b c'
+mapVals f (GMap x y) = GMap x (Map.map f y)
+
+mapKeys :: (Ord a') => (a -> a') -> GMap a b c -> GMap a' b c
+mapKeys f (GMap x y) = GMap (Map.mapKeys f x) y
+
+mapInnerKeys :: (Ord b') => (b -> b') -> GMap a b c -> GMap a b' c 
+mapInnerKeys f (GMap x y) = GMap (Map.map f x) (Map.mapKeys f y)
+
+keys :: GMap a b c -> [a]
+keys (GMap x _) = Map.keys x
+
+innerKeys :: (Ord b) => GMap a b c -> (Set.Set b, Set.Set b)
+innerKeys (GMap x y) = (Set.fromList (Map.elems x), Set.fromList (Map.keys y))
+
+elems :: GMap a b c -> [c]
+elems (GMap _ y) = Map.elems y
+
+empty :: GMap a b c
+empty = GMap Map.empty Map.empty
+
+insert :: (Ord a, Ord b) => a -> b -> c -> GMap a b c -> GMap a b c
+insert k1 k2 x (GMap m1 m2) = GMap (Map.insert k1 k2 m1) (Map.insert k2 x m2) 
+
+-- | Given `yIsX gmap x y`, the value `y` points to will be replaced with the
+-- value `x` points to. If `x` is not in `gmap`, then Nothing is returned. If
+-- `y` is in `gmap`, its previous link is silently lost.
+yIsX :: (Ord a) => GMap a b c -> a -> a -> Maybe (GMap a b c)
+yIsX (GMap m x) oldKey newKey = do
+  i <- Map.lookup oldKey m
+  return (GMap (Map.insert newKey i m) x)
+
+lookup :: (Ord a, Ord b) => a -> GMap a b c -> GMapRet c
+lookup k1 (GMap m1 m2) =
+  case Map.lookup k1 m1 of
+    Nothing -> GMapNoFst
+    (Just k2) -> case Map.lookup k2 m2 of 
+      Nothing -> GMapNoSnd
+      (Just x) -> GMapJust x
