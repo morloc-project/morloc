8864106431f85ca14bf8b0aa68c7d50c1e20dac8
Zebulun Arendsee <zbwrnz@gmail.com>
2023-07-12 06:56:02 -0500

    Add test for path shadowing bug

---
+++ test-suite/golden-tests/path-shadowing/bar/main.hpp
@@ -0,0 +1,8 @@
+#ifndef __BAR_MAIN_HPP__
+#define __BAR_MAIN_HPP__
+
+double add (double x, double y){
+    return x + y;
+}
+
+#endif
---
+++ test-suite/golden-tests/path-shadowing/baz/main.hpp
@@ -0,0 +1,8 @@
+#ifndef __BAZ_MAIN_HPP__
+#define __BAZ_MAIN_HPP__
+
+double mul (double x, double y){
+    return x * y;
+}
+
+#endif
