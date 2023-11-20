a02e3f79ce7435a64c0048f70c48d41f66211a90
Zebulun Arendsee <zbwrnz@gmail.com>
2023-11-19 20:00:24 -0600

    Fixed lazy ifelse and better plots
    There is still a bug, though. The clades are not being pulled correctly.
    This is probably a bug in bio.tree.pull. The filtering, unique, and
    checks all look fine.

--- demos/02_flu/plot-tree.R
+++ demos/02_flu/plot-tree.R
@@ -1,7 +1,8 @@
 library(ape)
 
 plotTree <- function(outputpdffilename, tree) {
-  pdf(outputpdffilename)
-  plot(tree)
-  dev.off()
+  pdf(outputpdffilename, width = 8, height = length(tree$tip.label) * 0.2)
+  par(cex = 0.7)  # Adjusts the label font size for readability
+  plot(tree, show.tip.label = TRUE, cex = 0.7)  # Plots the tree with tip labels
+  dev.off()  # Closes the PDF device
 }
