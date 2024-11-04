2db5587ddfe2e8a80926858f2b143350b4a5958e
Zebulun Arendsee <arendsee@users.noreply.github.com>
2024-11-03 22:01:13 -0500

    Update benchmarks
    The Python serialization was greatly improved leading a 4-fold
    improvement in interop with large string data. Moving to memory-mapped
    files should improve this even further. R suffers a constant 300ms
    penalty for the extra R instance (the host). A V-Lang re-implementation
    of the nexus would shave a constant 50ms or so from all these times.
    
     +------------------+-------------+------------+------------+-------------+
     | benchmark        | msgpack     | v2 packets | v1 sockets | v0          |
     +------------------+-------------+------------+------------+-------------+
     | cZeroBaseline    |   53 ± 1.6  |   48 ± 3.9 |   52 ± 3.6 |   44 ± 4.5  |
     | pZeroBaseline    |   94 ± 3.2  |  107 ± 8.0 |  102 ± 5.9 |   82 ± 4.0  |
     | pZeroFromForeign |   99 ± 1.5  |  107 ± 4.3 |  122 ± 65  |   89 ± 6.7  |
     | pZeroToForeign   |   98 ± 2.7  |  107 ± 4.1 |  118 ± 62  |   90 ± 5.7  |
     | rZeroBaseline    |  678 ± 8.1  |  850 ± 7.9 |  640 ± 80  |  312 ± 5.5  |
     | rZeroFromForeign |  672 ± 5.1  |  846 ± 5.1 | 1017 ± 253 |  345 ± 11.9 |
     | rZeroToForeign   |  660 ± 3.3  |  853 ± 7.2 |  950 ± 366 |  335 ± 6.6  |
     +------------------+-------------+------------+------------+-------------+
     | cTenBaseline     |   79 ± 1.8  |   95 ± 3.2 |  231 ± 85  |   98 ± 5.7  |
     | pTenBaseline     |  125 ± 3.2  |  152 ± 4.3 |  202 ± 71  |  133 ± 4.9  |
     | pTenFromForeign  |  809 ± 160  | 2146 ± 20  | 4867 ± 606 | 2232 ± 49   |
     | pTenToForeign    |  495 ± 3.9  | 1991 ± 13  | 2059 ± 217 | 2041 ± 44   |
     | rTenBaseline     | 1356 ± 9.2  | 1499 ± 12  | 2080 ± 320 | 1099 ± 34   |
     | rTenFromForeign  | 1527 ± 125  | 4349 ± 104 | 4213 ± 51  | 4191 ± 418  |
     | rTenToForeign    | 1646 ± 22   | 3908 ± 53  | 3858 ± 87  | 3529 ± 144  |
     +------------------+-------------+------------+------------+-------------+
     | rMarginalCost1   |  680 ± 4.9  |  857 ± 12  |  662 ± 8.3 |  328 ± 7.9  |
     | rMarginalCost2   |  774 ± 6.6  | 1150 ± 40  |  859 ± 10  |  637 ± 22   |
     | rMarginalCost3   |  863 ± 5.7  | 1429 ± 42  | 1051 ± 22  |  931 ± 55   |
     | rMarginalCost4   | 1050 ± 2.7  | 1732 ± 8.9 | 1258 ± 15  | 1221 ± 38   |
     | pMarginalCost1   |  107 ± 0.9  |  104 ± 4.6 |  104 ± 4.9 |   88 ± 4.4  |
     | pMarginalCost2   |  114 ± 1.7  |  113 ± 5.9 |  110 ± 3.9 |  124 ± 4.1  |
     | pMarginalCost3   |  117 ± 1.9  |  115 ± 4.6 |  113 ± 4.6 |  166 ± 4.9  |
     | pMarginalCost4   |  120 ± 0.7  |  118 ± 5.3 |  118 ± 4.0 |  214 ± 11   |
     +------------------+-------------+------------+------------+-------------+
     | pMap1000         | 2.68 ± 0.04 | 3.2 ± 0.02 |  4.6 ± 0.4 | 47.0 ± 5.1  |
     +------------------+-------------+------------+------------+-------------+
    
    This table shows benchmarks for 4 different versions of morloc. The
    latest is on the left.

--- test-suite/executable-benchmark/serial-interop/test.sh
+++ test-suite/executable-benchmark/serial-interop/test.sh
@@ -4,11 +4,11 @@ set -e
 
 morloc make foo.loc
 
-# hyperfine \
-#   -w 5 \
-#   -L test pZeroBaseline,rZeroBaseline,cZeroBaseline,pTenBaseline,rTenBaseline,cTenBaseline,pZeroToForeign,pZeroFromForeign,rZeroToForeign,rZeroFromForeign,pTenToForeign,pTenFromForeign,rTenToForeign,rTenFromForeign,rMarginalCost1,rMarginalCost2,rMarginalCost3,rMarginalCost4,pMarginalCost1,pMarginalCost2,pMarginalCost3,pMarginalCost4 \
-#   --export-markdown stats.markdown \
-#   --export-csv stats.csv \
-#   "./nexus.py {test}"
+hyperfine \
+  -w 5 \
+  -L test pZeroBaseline,rZeroBaseline,cZeroBaseline,pTenBaseline,rTenBaseline,cTenBaseline,pZeroToForeign,pZeroFromForeign,rZeroToForeign,rZeroFromForeign,pTenToForeign,pTenFromForeign,rTenToForeign,rTenFromForeign,rMarginalCost1,rMarginalCost2,rMarginalCost3,rMarginalCost4,pMarginalCost1,pMarginalCost2,pMarginalCost3,pMarginalCost4 \
+  --export-markdown stats.markdown \
+  --export-csv stats.csv \
+  "./nexus.py {test}"
 
-hyperfine -w 5 "./nexus.py mapManyPC 5 long-list"
+hyperfine -w 5 "./nexus.py mapManyPC 5 long-list.json"
