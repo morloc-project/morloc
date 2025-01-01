c86c1c77702e49986925f26f78571e6fc2d11dd9
Zebulun Arendsee <arendsee@users.noreply.github.com>
2025-01-01 18:35:13 -0500

    Clean up tests and run benchmarks
    The key change is seen in the pTen* functions, where a speedup of ~2X is
    observed. The benchmark doesn't show it well, but there is also a major
    increase in R speed. The interop for R is so fast that the R code
    actually runs faster when calling the C code. Though this may just be
    because the stringi operation is slow. I probably need a better
    benchmark. The shared memory handline is VERY far from optimized. In the
    future I should be able to avoid copying in many circumstances, but I
    think I am on the right track.
    
    I should also point out that the benchmarks are not entirely fair, since
    I've changed the C libraries considerably and added other improvements.
    A better benchmark would be if I added an option to turn off shm. Then I
    could compare with/without it in the same version of morloc.
    
     +------------------+---------------+---------------+---------------------------+
     | benchmark        | shm           | globals=FALSE | msgpack     | v0          |
     +------------------+---------------+---------------+---------------------------+
     | cZeroBaseline    | 52.2   ± 0.7  |   54.1 ± 1.5  |   53 ± 1.6  |   44 ± 4.5  |
     | pZeroBaseline    | 91.6   ± 3.1  |   96.8 ± 3.5  |   94 ± 3.2  |   82 ± 4.0  |
     | pZeroFromForeign | 96.2   ± 3.0  |  100.2 ± 1.4  |   99 ± 1.5  |   89 ± 6.7  |
     | pZeroToForeign   | 99.9   ± 4.1  |  101.3 ± 3.2  |   98 ± 2.7  |   90 ± 5.7  |
     | rZeroBaseline    | 579.3  ± 3.9  |  580.1 ± 6.2  |  678 ± 8.1  |  312 ± 5.5  |
     | rZeroFromForeign | 573.0  ± 2.9  |  568.6 ± 3.2  |  672 ± 5.1  |  345 ± 11.9 |
     | rZeroToForeign   | 571.5  ± 2.8  |  571.4 ± 4.5  |  660 ± 3.3  |  335 ± 6.6  |
     +------------------+---------------+---------------+-------------+-------------+
     | cTenBaseline     | 82.6   ± 3.0  |   79.7 ± 3.2  |   79 ± 1.8  |   98 ± 5.7  |
     | pTenBaseline     | 124.0  ± 1.8  |  126.3 ± 4.1  |  125 ± 3.2  |  133 ± 4.9  |
     | pTenFromForeign  | 238.8  ± 1.6  |  706.5 ± 172  |  809 ± 160  | 2232 ± 49   |
     | pTenToForeign    | 217.1  ± 7.8  |  509.0 ± 3.5  |  495 ± 3.9  | 2041 ± 44   |
     | rTenBaseline     | 1271.3 ± 12.1 | 1266.8 ± 2.9  | 1356 ± 9.2  | 1099 ± 34   |
     | rTenFromForeign  | 1033.5 ± 5.6  | 1333.8 ± 122  | 1527 ± 125  | 4191 ± 418  |
     | rTenToForeign    | 1302.1 ± 22.0 | 1538.6 ± 4.8  | 1646 ± 22   | 3529 ± 144  |
     +------------------+---------------+---------------+-------------+-------------+
     | rMarginalCost1   | 580.4  ± 4.5  |  581.7 ± 7.0  |  680 ± 4.9  |  328 ± 7.9  |
     | rMarginalCost2   | 624.6  ± 2.8  |  621.9 ± 2.9  |  774 ± 6.6  |  637 ± 22   |
     | rMarginalCost3   | 666.6  ± 6.5  |  668.7 ± 18.5 |  863 ± 5.7  |  931 ± 55   |
     | rMarginalCost4   | 741.5  ± 3.3  |  737.9 ± 12.1 | 1050 ± 2.7  | 1221 ± 38   |
     | pMarginalCost1   | 102.3  ± 0.9  |  108.0 ± 0.9  |  107 ± 0.9  |   88 ± 4.4  |
     | pMarginalCost2   | 108.6  ± 2.7  |  114.9 ± 1.4  |  114 ± 1.7  |  124 ± 4.1  |
     | pMarginalCost3   | 111.9  ± 2.8  |  119.2 ± 1.7  |  117 ± 1.9  |  166 ± 4.9  |
     | pMarginalCost4   | 114.6  ± 1.4  |  122.4 ± 0.7  |  120 ± 0.7  |  214 ± 11   |
     +------------------+---------------+---------------+-------------+-------------+
     | pMap1000         |               |  2.7          | 2.68 ± 0.04 | 47.0 ± 5.1  |
     | rMap1000         |               | 23.8          | 70.0        | -           |
     +------------------+---------------+---------------+-------------+-------------+

--- test-suite/executable-benchmark/serial-interop/foo.R
+++ test-suite/executable-benchmark/serial-interop/foo.R
@@ -1,8 +1,10 @@
 rid <- function(x) x
 
-# Generate a 1 MB random string
+# Generate an n MB random string
 nmb <- function(n){
   stringi::stri_dup("x", n * 1024 * 1024)
 }
 
 incr <- function(x, y) x + y
+
+addr <- function(x, y) x + y
