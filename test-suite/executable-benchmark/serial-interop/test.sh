afb56f1e203867850e8c5bfe8741e08247563821
Zebulun Arendsee <arendsee@users.noreply.github.com>
2025-04-20 19:44:09 -0400

    Update benchmark code
    With the new C nexus and centralized morloc.h code, the ~50ms overhead
    from the python nexus is removed. Other code is roughly the same, other
    than a possible decrease in R performance.
    
     +------------------+---------------+---------------+
     | benchmark        | c nexus       | shm           |
     +------------------+---------------+---------------+
     | cZeroBaseline    | 5.8    ± 1.0  | 52.2   ± 0.7  |
     | pZeroBaseline    | 49.2   ± 9.7  | 91.6   ± 3.1  |
     | pZeroFromForeign | 51.0   ± 9.9  | 96.2   ± 3.0  |
     | pZeroToForeign   | 52.6   ± 10.4 | 99.9   ± 4.1  |
     | rZeroBaseline    | 584.0  ± 8.7  | 579.3  ± 3.9  |
     | rZeroFromForeign | 583.2  ± 11.7 | 573.0  ± 2.9  |
     | rZeroToForeign   | 581.0  ± 3.9  | 571.5  ± 2.8  |
     +------------------+---------------+---------------+
     | cTenBaseline     | 33.6   ± 0.4  | 82.6   ± 3.0  |
     | pTenBaseline     | 88.1   ± 10.2 | 124.0  ± 1.8  |
     | pTenFromForeign  | 193.2  ± 9.9  | 238.8  ± 1.6  |
     | pTenToForeign    | 165.8  ± 10.7 | 217.1  ± 7.8  |
     | rTenBaseline     | 1301.4 ± 7.6  | 1271.3 ± 12.1 |
     | rTenFromForeign  | 1067.2 ± 3.6  | 1033.5 ± 5.6  |
     | rTenToForeign    | 1276.4 ± 3.4  | 1302.1 ± 22.0 |
     +------------------+---------------+---------------+
     | rMarginalCost1   | 583.9  ± 11.3 | 580.4  ± 4.5  |
     | rMarginalCost2   | 632.5  ± 2.1  | 624.6  ± 2.8  |
     | rMarginalCost3   | 724.1  ± 19.3 | 666.6  ± 6.5  |
     | rMarginalCost4   | 879.8  ± 14.7 | 741.5  ± 3.3  |
     | pMarginalCost1   | 50.6   ± 11.0 | 102.3  ± 0.9  |
     | pMarginalCost2   | 49.4   ± 7.6  | 108.6  ± 2.7  |
     | pMarginalCost3   | 49.9   ± 6.6  | 111.9  ± 2.8  |
     | pMarginalCost4   | 51.0   ± 7.7  | 114.6  ± 1.4  |
     +------------------+---------------+---------------+
    
    The C pool crashes in the long list test. What is happening is that the
    thousand or so values are all forked in a row which causes file
    descriptors to run out. I'll need to add some sort of queue that
    prevents this.

--- test-suite/executable-benchmark/serial-interop/test.sh
+++ test-suite/executable-benchmark/serial-interop/test.sh
@@ -8,17 +8,17 @@ morloc make foo.loc
 hyperfine \
   -w 10 \
   -L test pTenBaseline,rTenBaseline,cTenBaseline \
-  "./nexus.py {test}"
+  "./nexus {test}"
 
 hyperfine \
   -w 5 \
   -L test cZeroBaseline,pZeroBaseline,pZeroFromForeign,pZeroToForeign,rZeroBaseline,rZeroFromForeign,rZeroToForeign,cTenBaseline,pTenBaseline,pTenFromForeign,pTenToForeign,rTenBaseline,rTenFromForeign,rTenToForeign,rMarginalCost1,rMarginalCost2,rMarginalCost3,rMarginalCost4,pMarginalCost1,pMarginalCost2,pMarginalCost3,pMarginalCost4 \
   --export-markdown stats.markdown \
   --export-csv stats.csv \
-  "./nexus.py {test}"
+  "./nexus {test}"
 
-# hyperfine -w 5 -L test memtest "./nexus.py {test} medium-list.json"
+# hyperfine -w 5 -L test memtest "./nexus {test} medium-list.json"
 
-# hyperfine -w 5 -L test mapManyPCP,mapManyPCR "./nexus.py {test} 5 long-list.json"
+# hyperfine -w 5 -L test mapManyPCP,mapManyPCR "./nexus {test} 5 long-list.json"
 
-# hyperfine -w 5 -L test mapManyPCR "./nexus.py {test} 5 long-list.json"
+# hyperfine -w 5 -L test mapManyPCR "./nexus {test} 5 long-list.json"
