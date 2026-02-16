595a765376c56bc14495d6d808fa26c16d936eea
Zebulun Arendsee <zbwrnz@gmail.com>
2026-02-16 17:36:25 -0500

    Add stubby test harness

---
+++ test-suite/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+./concurrency-tests/run-tests.sh
+
+./stress/run-all.sh
+
+./typecheck-benchmark/run-benchmarks.sh
+
+./daemon-tests/run-tests.sh
+
+stack test
+
+echo "libmorloc tests not defined yet"
+# cmorloc-tests:
+
+echo "error-message tests not defined yet"
+# error-message-tests:
+
+echo "executable benchmarks not yet up" 
+# executable-benchmark/distributed:
+# executable-benchmark/parallel-interop:
+# executable-benchmark/serial-interop:
